20db62870fefa97e9698a5122c163ce536df13f3
Alex Morson <amorson@conncoll.edu>
2018-08-06 16:58:20 -0500

    can successfully add initial vegetable seed to the vegetables table in databse

--- models.js
+++ models.js
@@ -1,7 +1,6 @@
 const {Sequelize} = require('sequelize');
 
 const db = new Sequelize('postgres://localhost:5432/plantr');
-module.exports = db;
 
 const Gardener = db.define('gardener', {
     name: Sequelize.STRING,
@@ -27,7 +26,4 @@ Plot.belongsToMany(Vegetable, {through: 'vegetable_plot'});
 
 Gardener.belongsTo(Vegetable, {as: 'favorite_vegetable'});
 
-const carrot = Vegetable.findOrCreate({where: {name: 'carrot', color: 'orange', planted_on: '1995-12-17T03:24:00'}});
-const tomato = Vegetable.findOrCreate({where: {name: 'tomato', color: 'red', planted_on: '1995-12-17T03:24:00'}});
-
-Promise.all([carrot, tomato]).then(console.log())
+module.exports = {db, Gardener, Plot, Vegetable};
--- seed.js
+++ seed.js
@@ -1,7 +1,23 @@
-const db = require('./models.js');
+const {db, Gardener, Plot, Vegetable} = require('./models.js');
+
+const initialSeed = [
+    {
+        name: 'carrot',
+        color: 'orange',
+        planted_on: Date.now()
+    },
+    {
+        name: 'tomato',
+        color: 'red',
+        planted_on: Date.now()
+    }
+];
 
 db.sync({force: true})
-    .then(() => console.log('Database synced'))
+    .then(() => {
+        return Vegetable.bulkCreate(initialSeed)
+        .then(()=> {console.log('Database synced');})
+    })
     .catch(err => {
         console.log('Something went wrong. ');
         console.log(err);
